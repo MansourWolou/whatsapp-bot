0fc2e03016525fb37a3012617a23bdad7e1a45ab
bertyn99 <bboulikou@gmail.com>
2022-10-30 23:18:09 +0100

    ajout dto login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { LoginAuthDto } from './dto/login-auth.dto';
 import { RegisterAuthDto } from './dto/resgister-auth.dto';
+import { User, Prisma } from '@prisma/client';
 
 @Injectable()
 export class AuthService {
-  login(loginAuthDto: LoginAuthDto) {
+  login(loginInput: Prisma.UserCreateInput) {
     throw new Error('Method not implemented.');
   }
-  register(registerAuthDto: RegisterAuthDto) {
+  register(registerInput: Prisma.UserCreateInput) {
     throw new Error('Method not implemented.');
   }
 }
--- src/auth/dto/login-auth.dto.ts
+++ src/auth/dto/login-auth.dto.ts
@@ -1,1 +1,9 @@
-export class LoginAuthDto {}
+import { Prisma } from '@prisma/client';
+import { IsNotEmpty, IsString } from 'class-validator';
+export class LoginUserDto {
+  @IsNotEmpty()
+  readonly login: string;
+
+  @IsNotEmpty()
+  readonly password: string;
+}
