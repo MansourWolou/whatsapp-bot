60816f65eb85b027ea9821ddfc006e3984af908e
MansourWolou <Wolou0444@gmail.com>
2022-10-30 21:45:02 +0100

    webhook config

--- src/waw/waw.controller.ts
+++ src/waw/waw.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query, Req,Res } from '@nestjs/common';
 import { WawService } from './waw.service';
 
 @Controller('waw')
 export class WawController {
     constructor(private readonly wawService: WawService){}
 
-    @Get("webhook")
+    @Get("/")
     getHook() : string{
         return this.wawService.getHook();
     }
+    @Get('webhook')
+    setUpWebhook(@Query('hub.mode') mode:any,
+                @Query('hub.verify_token')token : any,
+                @Query('hub.challenge') challenge:any ,
+                @Req() req,@Res() res) {
+                   return this.wawService.setUpWebhook(mode,token,challenge,req,res);
+    }
+    
 }
--- src/waw/waw.service.ts
+++ src/waw/waw.service.ts
@@ -1,9 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, Req, Res } from '@nestjs/common';
 
 @Injectable()
 export class WawService {
     getHook() : string{
         return "this is a webhook oooo";
+    }
+    setUpWebhook(mode: any,token: any,challenge: any,req:Request,res:Response){
+        console.log(mode,token,challenge);
+        
+        /** UPDATE YOUR VERIFY TOKEN
+        This will be the Verify Token value when you set up webhook**/
+        const VERIFY_TOKEN = "hello7";
+
+        // Check if a token and mode were sent
+        if (mode && token) {
+        
+          // Check the mode and token sent are correct
+          if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+
+            // Respond with 200 OK and challenge token from the request
+            console.log('WEBHOOK_VERIFIED');
+            return challenge;
+
+          } else {
+            // Responds with '403 Forbidden' if verify tokens do not match
+            throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
+          }
+        }
+    }
+    postToWebhook(){
+
     }
 }
 
